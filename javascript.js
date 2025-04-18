eb6217c0500591fad4808ac5fcc3684199cd7a06
spiderzoast <161398574+spiderzoast@users.noreply.github.com>
2025-04-18 00:34:20 -0300

    Implement operate function

--- javascript.js
+++ javascript.js
@@ -14,3 +14,18 @@ function divide(a, b) {
 	return a / b;
 };
 
+function operate(operator, a, b) {
+  switch (operator) {
+    case '+':
+      return add(a, b);
+    case '-':
+      return subtract(a, b);
+    case '*':
+      return multiply(a, b);
+    case '/':
+      return divide(a, b);
+    default:
+      return "Invalid operator";
+  }
+}
+
