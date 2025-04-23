6e19a03b88518ec174efef0d47a53aeaffb98133
spiderzoast <161398574+spiderzoast@users.noreply.github.com>
2025-04-23 02:10:30 -0300

    Implement more edge cases for the calculator inputs

--- javascript.js
+++ javascript.js
@@ -1,24 +1,32 @@
-let num1;
-let num2;
-let operator;
-
 const display = document.querySelector(".calculator-display p");
 
 document.querySelectorAll('.calculator-row button').forEach(button => {
   button.addEventListener('click', () => {
-    const input = button.value || button.textContent;
+    const input = button.textContent;
+    const operators = ['+', '−', '×', '/'];
+    const lastChar = display.textContent.slice(-1);
     if (input === "=") {
-      const arr = display.textContent.match(/([0-9.]+)([+\-×/])([0-9.]+)/);
-      num1 = Number(arr[1]);
-      operator = arr[2];
-      num2 = Number(arr[3]);
-      display.textContent = operate(operator, num1, num2);
+      const match = display.textContent.match(/([0-9.]+)([+\−×/])([0-9.]+)/);
+      if (match) {
+        const [expression, num1, operator, num2] = match;
+        const remaining = display.textContent.slice(expression.length); 
+        const result = operate(operator, +num1, +num2);
+        if (remaining) {
+          display.textContent = result + remaining;
+        } else {
+          display.textContent = result;
+        };
+      };
     } else if (input === "C") {
       display.textContent = "0";
     } else if (display.textContent === "0") {
       display.textContent = input;
     } else {
+      if (operators.includes(lastChar) && operators.includes(input)) {
+        display.textContent = display.textContent.slice(0, -1) + input; 
+      } else {
       display.textContent += input; 
+      };
     };
   });
 });
@@ -27,9 +35,9 @@ function operate(operator, num1, num2) {
   switch (operator) {
     case '+':
       return num1 + num2;
-    case '-':
+    case '−':
       return num1 - num2;
-    case '*':
+    case '×':
       return num1 * num2;
     case '/':
       if (num2 === 0){
